1d35a16a1e211ff12feeeebd8656308cc9dd5a3f
Juan Camilo <camilo@gmail.com>
2024-02-20 13:21:56 -0500

    cambiando version 4

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const body = document.querySelector('body')
 const name = document.querySelector('#name')
 const doc = document.querySelector('#doc')
 
-version.textContent = 'Version 3'
+version.textContent = 'Version 4'
 
 const btn = document.querySelector('button')
 btn.addEventListener('click', e => {
     const n = document.createElement('section')
     n.innerHTML = `
-        ${name}
-        ${doc}
+        ${name.value}
+        ${doc.value}
     `
     body.appendChild(n)
 })
